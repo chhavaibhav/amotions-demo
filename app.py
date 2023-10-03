aeeefed4f77689ee7f32bb91401c7dd05302d710
vchhajed <vchhajed@buffalo.edu>
2023-10-02 17:50:35 -0700

    test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
+from langchain.vectorstores import Chroma
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
     SystemMessagePromptTemplate,
